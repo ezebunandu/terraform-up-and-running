d5118f771e2b0debae63dbfaf732fc00b2aa4f1f
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2023-02-22 06:26:30 -0700

    add prod infra using modules

---
+++ terraform/03-terraform-state/file-layout-example/prod/data-stores/mysql/main.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_db_instance" "example" {
+  identifier_prefix   = "terraform-up-and-running-prod"
+  engine              = "mysql"
+  allocated_storage   = 10
+  instance_class      = "db.t2.micro"
+  skip_final_snapshot = true
+  db_name             = "prod_db"
+
+  username = var.db_username
+  password = var.db_password
+}
+
+terraform {
+  backend "s3" {
+    bucket = "hezebonica-terraform-up-and-running-state"
+    key    = "prod/data-stores/mysql/terraform.tfstate"
+    region = "us-east-2"
+
+    dynamodb_table = "hezebonica-terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
---
+++ terraform/03-terraform-state/file-layout-example/prod/services/webserver-cluster/main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "us-east-2"
+}
+# Partial configuration. The other settings will be 
+# passed in from a file via -backend-config arguments to 'terraform init'
+terraform {
+  backend "s3" {
+    key            = "prod/services/webserver-cluster"
+    region         = "us-east-2"
+    dynamodb_table = "hezebonica-terraform-up-and-running-locks"
+  }
+}
+
+module "webserver_cluster" {
+  source                 = "../../../modules/services/webserver-cluster"
+  cluster_name           = "webservers-prod"
+  db_remote_state_bucket = "hezebonica-terraform-up-and-running-state"
+  db_remote_state_key    = "prod/data-stores/mysql/terraform.tfstate"
+}
